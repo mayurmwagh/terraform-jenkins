97b86ca7d0b069ba7fc8c0a15d6467d25765e473
mayurwagh <mayurwagh1412@gmail.com>
2025-05-07 21:36:11 +0530

    enviromnet variable is added

--- variable-jenkins.groovy
+++ variable-jenkins.groovy
@@ -1,5 +1,9 @@
 pipeline {
     agent any 
+    environment {
+        Prod_Ip = '192.168.9.0'
+        UAT_Ip    = '192.186.2.1'
+    }
     stages {
         stage('code-pull'){
             steps {
@@ -15,6 +19,19 @@ pipeline {
         stage('print-default-variable'){
             steps {
                 sh 'printenv' 
+                
+            }
+        }
+        stage('print-default-variable'){
+            steps {
+                sh 'printenv' 
+                
+            }
+        }
+        stage('Printing-environment-variable'){
+            steps {
+                echo "prod ip is {$Prod_Ip}"
+                echo "prod ip is {$UAT_Ip}"
                 
             }
         }
