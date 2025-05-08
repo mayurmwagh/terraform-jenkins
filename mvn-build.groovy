14cce19be7281bddb158ee7ca9e9a5f77968d372
mayurwagh <mayurwagh1412@gmail.com>
2025-05-08 21:26:46 +0530

    tomcat stage is added

--- mvn-build.groovy
+++ mvn-build.groovy
@@ -23,7 +23,11 @@ pipeline {
         }
         stage('workspace'){
             steps{
-                echo $workspace
+                sh '''
+                    curl https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.104/bin/apache-tomcat-9.0.104.tar.gz
+                    tar -xvf tomcat-9.0.104.tar.gz -C /opt/tomcat
+                    cp /home/ubuntu/workspace/mvn-project/target/*.war /opt/tomcat/webapps
+                '''
                    
             }
         }
